35052857d7393fe66ccd8a07816658428350bd07
Viktor Szépe <viktor@szepe.net>
2020-07-24 07:07:23 +0200

    Add rocket fuel

--- qm-rest-api.php
+++ qm-rest-api.php
@@ -1,25 +1,36 @@
 <?php
+
 /*
-Plugin Name: Query Monitor - REST API
-Description: Adds query info to REST API headers
+Plugin Name: Query Monitor REST API
+Description: Adds query info to REST API headers.
 Author: Mike Nelson
 Version: 0.0.1
 */
+
 add_filter(
 	'rest_post_dispatch',
-	function(WP_REST_Response $rest_response, WP_REST_Server $server, $request){
-		$rest_response->header('mike','the guy');
+	function (WP_REST_Response $rest_response, WP_REST_Server $server, $request) {
 		global $wpdb;
-		if($wpdb instanceof QM_DB){
-			$queries = $wpdb->queries;
-			$rest_response->header('Query-Monitor-Status','wp-content/db.php working. Query data added to response headers.');
-			$x = 0;
-			foreach($queries as $query){
-				$rest_response->header('Query-Monitor-Query-' . $x++,sprintf('Time:%f,Query "%s"',$query[1],$query[0]));
+
+		$rest_response->header('X-Mike', 'the guy');
+		if ($wpdb instanceof QM_DB) {
+			$rest_response->header(
+				'Query-Monitor-Status',
+				'wp-content/db.php working. Query data added to response headers.'
+			);
+			foreach ($wpdb->queries as $query_index => $query) {
+				$rest_response->header(
+					'Query-Monitor-Query-' . strval($query_index),
+					sprintf('Time:%f,Query "%s"', $query[1], $query[0])
+				);
 			}
 		} else {
-			$rest_response->header('Query-Monitor-Status','wp-content/db.php not working. See https://github.com/johnbillion/query-monitor/wiki/db.php-Symlink');
+			$rest_response->header(
+				'Query-Monitor-Status',
+				'wp-content/db.php not working. See https://github.com/johnbillion/query-monitor/wiki/db.php-Symlink'
+			);
 		}
+
 		return $rest_response;
 	},
 	10,
